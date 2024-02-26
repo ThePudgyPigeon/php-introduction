de03537806a63133d8c069727dcd6ce16278e23c
Samantha Stinchcomb <123195400+ThePudgyPigeon@users.noreply.github.com>
2024-02-26 18:25:36 +0000

    for looped to flip coin 1000 times and set counters for heads and tails

--- 01_03b/index.php
+++ 01_03b/index.php
@@ -1,4 +1,4 @@
-<style>
+<!-- <style>
 	.coin {
 		background: #999999;
 		color: #333333;
@@ -9,16 +9,26 @@
 		font-weight: bold;
 		width: 50px;
 	}
-</style>
+</style> -->
 
 <?php
 
-function flip() {
-	return ( 0 == rand(0,1) ) ? 'H' : 'T';
+function flip()
+{
+	return (0 == rand(0, 1)) ? 'H' : 'T';
 }
 
+$H = 0;
+$T = 0;
+
+for ($i = 0; $i < 1000; $i++) {
+	$flip = flip();
+	${$flip}++;
+}
+
+echo '<p>Heads was flipped ' . $H / 10 . '% of the time. Tails was flipped ' . $T / 10 . '%.'
 ?>
 
-<div class="coin">
+<!-- <div class="coin">
 	<?php echo flip(); ?>
-</div>
+</div> -->
