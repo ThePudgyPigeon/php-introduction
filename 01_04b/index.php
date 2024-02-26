5939f4d518f842b53f866e67f352bc69eeedaad2
Samantha Stinchcomb <123195400+ThePudgyPigeon@users.noreply.github.com>
2024-02-26 18:35:01 +0000

    learned how to create a singleton class

--- 01_04b/index.php
+++ 01_04b/index.php
@@ -1,29 +1,48 @@
 <?php
-class MyConnect {
+class MyConnect
+{
 	private $db_name;
 	private $username;
 	private $password;
 	private $host;
-	
-	function __construct($name, $username, $password) {
+
+	//creating a singleton class so it is only instantiated once
+	private static $connection;
+
+
+	//private so it can only  be accessed inside of the class
+	private function __construct($name, $username, $password)
+	{
 		$this->db_name = $name;
 		$this->username = $username;
 		$this->password = $password;
-		$this->host = 'localhost';	
+		$this->host = 'localhost';
 	}
-	
-	public function get_info( $query ) {
-		//STUB
+
+	//this is how we can create our singleton!
+	public static function open_connection($name, $username, $password)
+	{
+		if (!isset(self::$connection)) {
+			self::$connection = new MyConnect($name, $username, $password);
+		}
+		return self::$connection;
 	}
-	
-	public function set_info( $query ) {
+
+	public function get_info($query)
+	{
 		//STUB
 	}
 
-	public function get_db_name() {
+	public function set_info($query)
+	{
+		//STUB
+	}
+
+	public function get_db_name()
+	{
 		return $this->db_name;
 	}
 }
 
-$db = new MyConnect( 'people', 'joe', 'hello_there' );
+$db = MyConnect::open_connection('people', 'joe', 'hello_there');
 echo $db->get_db_name();
